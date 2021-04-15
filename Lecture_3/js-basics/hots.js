b9c7260198ab591554e1930f2d05a26e1bd56ad6
Apoorvgarg <apoorvgarg.21@gmail.com>
2021-04-15 23:50:59 +0530

    intro to Higher order function

---
+++ Lecture_3/js-basics/hots.js
@@ -0,0 +1,24 @@
+function greet(name){
+    return 'Hello ' + name
+}
+function createGreeter(greeting){
+
+    function greet(name){
+        // console.log(greeting, name)
+        console.log(greeting, name())
+    }
+
+    return greet
+}
+
+function getName() {
+    return document.getElementById('namebox').value
+}
+let g1 = createGreeter('Good Morning')
+let g2 = createGreeter('Good Evening')
+
+// console.log(typeof g1)
+console.log(greet('Arnav'))
+console.log(greet('Prateek'))
+console.log(g1('Apoorv'))
+console.log(g2('Garg'))
