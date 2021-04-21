e0b7f72aa00e1e2d7356dc3bf18e1671b29d3879
Apoorvgarg <apoorvgarg.21@gmail.com>
2021-04-22 00:20:00 +0530

    string literals explained

---
+++ Lecture_4/string-literals/script.js
@@ -0,0 +1,31 @@
+let s1 = 'He said, "this is so awesome!"'
+console.log(s1)
+
+let s2 = "He said ,\"This is so awesome! \" "
+console.log(s2)
+
+let s3 = "He said, 'This is so awesome!'"
+console.log(s3)
+// backtick
+let s4 = `He said, "This is so awesome!"`
+console.log(s4)
+
+let s5 = "This string is \n in multiple line"
+console.log(s5)
+// backtick
+let s6 = `This string
+is in
+multiple line`
+console.log(s6)
+
+let name = `john`
+
+let g1 = 'single quotation: Good morning '  + name
+console.log(g1)
+
+let g2  = `backtick: Good Morning ${name}`
+console.log(g2)
+
+let s7 = `10 + 2 = ${10+2}`
+console.log(s7)
+
