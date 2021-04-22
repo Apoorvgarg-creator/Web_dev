21b649df1e7844d00c44239c1aa1837bad59ed28
Apoorvgarg-creator <APOORVGARG.21@GMAIL.COM>
2021-04-22 19:31:28 +0530

    callstack

---
+++ JSDeepDive/callstack.js
@@ -0,0 +1,27 @@
+function ceo () {
+    console.log("Let's make a dent in the universe")
+    cto()
+}
+
+function cto () {
+    console.log("Let's make a gamechanging product")
+    vpengg()
+
+}
+
+function vpengg() {
+    console.log("Let's make a new Javascript framework")
+    techlead()
+}
+
+function techlead() {
+    console.log("Let's port angular to typescript")
+    developer()
+}
+
+function developer() {
+    console.log("Let's copy some code from stackoverflow")
+    throw new Error("The code didn't work !!!")
+}
+
+ceo()
