9233dc89841663150104d7db5a2c41caa37c92f0
Apoorvgarg-creator <APOORVGARG.21@GMAIL.COM>
2021-04-23 21:21:16 +0530

    traversal in DOM

---
+++ DOM/traversal/script.js
@@ -0,0 +1,15 @@
+// mi.parentElement
+// mi.parentElement.parentElement.previousElementSibling.children
+// mi.previousElementSibling
+// mi.nextElementSibling
+// mi.parentElement.parentElement.previousElementSibling  // li bee element
+// --> DOM traversal
+// mi.parentElement.parentElement.previousElementSibling.children[0].children[2]
+// mi.parentElement.remove()
+
+// let dee = temp1
+// dee
+// dee.appendChild(mi.parentElement)
+// mi.remove()
+// dee.previousElementSibling.previousElementSibling.children[0]
+// dee.previousElementSibling.previousElementSibling.children[0].appendchild
