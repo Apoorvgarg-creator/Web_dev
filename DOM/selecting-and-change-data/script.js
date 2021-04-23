2a6b71fb405ee1923c3d55edc9d4665c0444a62f
Apoorvgarg-creator <APOORVGARG.21@GMAIL.COM>
2021-04-23 19:08:56 +0530

    Document object Model (DOM)

---
+++ DOM/selecting-and-change-data/script.js
@@ -0,0 +1,35 @@
+// console.log(document)
+// console.log(document.body)
+// console.log(document.getElementsByTagName('p'))
+// console.log(document.getElementsByClassName('para'))
+// console.log(document.getElementById('inpbox'))
+
+// let p2 = document.getElementsByClassName('para')[1]
+
+// console.log(p2.innerText)
+// console.log(p2.innerHTML)
+// console.log(p2.outerHTML)
+
+// p2.innerText = "some new data" // webpage content will be changed !
+// p2.innerText = "hello <b> BOLD</b>"
+// p2.innerHTML = "hello <b> BOLD</b>"
+// p2.getAttribute('class')
+// p2.getAttribute('id')
+// p2.setAttribute('contenteditable',true)
+// p2.setAttribute('spellcheck',false)
+// p2.outerHTML
+
+// let i = document.getElementById('inpbox')
+
+// i.innerText
+// i.innerHTML
+// i.value
+// i.value = 'new stuff'
+
+// i.setAttribute('type','email')
+// i.validity
+
+// let btn  = document.getElementById('btn')
+
+// btn.innerHTML
+// btn.innerText
