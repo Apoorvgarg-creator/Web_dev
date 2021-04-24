849025f058acaa0260b6ea1dfc6c5743f542b982
Apoorvgarg-creator <APOORVGARG.21@GMAIL.COM>
2021-04-25 00:18:31 +0530

    batch task using Promise.all()

--- JS_Concurrency/helloSayerAsyncAwait.js
+++ JS_Concurrency/helloSayerAsyncAwait.js
@@ -13,12 +13,26 @@ function helloSayer(times,name){
 }
 
 
-async function task () {
-   await helloSayer(3,'Appy')
-   await helloSayer(2,'Naruto')
-   await  helloSayer(3,'luffy')
+// async function task () {
+//    await helloSayer(3,'Appy')
+//    await helloSayer(2,'Naruto')
+//    await  helloSayer(3,'luffy')
+//
+// }
+//
+// task()
+// helloSayer(5,'zoro')
 
-}
-
-task()
-helloSayer(5,'zoro')
+(async function (){
+    await Promise.all([
+    helloSayer(6,'Appy'),
+    helloSayer(2,'Naruto'),
+    helloSayer(3,'Luffy')
+    ])
+    console.log(`<----- first batch over ----->`)
+    await Promise.all([
+    helloSayer(4,'zoro'),
+    helloSayer(6,'Sasuke'),
+    helloSayer(2,'shikamaru')
+    ])
+})();
