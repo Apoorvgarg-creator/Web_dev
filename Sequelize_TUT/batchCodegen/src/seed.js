8c4eb5038e3376bef0c2b98d6aeca0a019bb56a5
Apoorvgarg-creator <APOORVGARG.21@GMAIL.COM>
2021-05-03 21:36:44 +0530

    Initializing values in the tables

---
+++ Sequelize_TUT/batchCodegen/src/seed.js
@@ -0,0 +1,39 @@
+const {db, Center, Season, Batch, Course, Teacher} = require('./models')
+
+
+const seed = async () => {
+    try {
+        db.sync({alter:true})
+        await Center.bulkCreate([
+            {id:`PP`, name:`Pritampura`,city:`New Delhi`},
+            {id:`DW`, name:`Dwarka`,city:`New Delhi`},
+            {id:`NO`, name:`Noida`,city:`Noida`},
+            {id:`DD`, name:`Dehradun`,city:`Dehradun`},
+            {id:`OL`, name:`Online`,city:`New Delhi`}
+
+
+        ],{
+            ignoreDuplicates: true
+        })
+        await Season.bulkCreate([
+            {id:`S`,name:`Summer`},
+            {id:`F`,name:`Fall`},
+            {id:`W`,name:`Winter`},
+            {id:`P`,name:`Spring`},
+        ],{
+            ignoreDuplicates:true
+        })
+
+        await Course.bulkCreate([
+            {id:`LP`,name:`Launchpad`},
+            {id:`CX`,name:`Crux`},
+            {id:`IB`,name:`Interview Bootcamp`},
+            {id:`AD`,name:`Android development`},
+            {id:`WD`,name:`Web Development`}
+        ],{
+            ignoreDuplicates:true
+        })
+    }catch (e) {
+        console.error(e)
+    }
+}
