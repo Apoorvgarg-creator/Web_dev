1d618c158635469c90a37ea9b13f315f4d2d9d68
Apoorvgarg-creator <APOORVGARG.21@GMAIL.COM>
2021-05-03 21:36:21 +0530

    connecting to the database

---
+++ Sequelize_TUT/batchCodegen/src/connection.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize')
+const  db = new Sequelize('sampledb1','sampleuser1','SamplePass@1',{
+    host: `localhost`,
+    dialect: `mysql`
+})
+
+db.authenticate().then(() => {
+    console.log("Connection successful !");
+}).catch((err) => {
+    console.error(err);
+})
+
+
+module.exports = {
+    db
+}
