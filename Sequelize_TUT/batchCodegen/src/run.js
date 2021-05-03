50afe6ef4f5ed0bdca1e5a1d47d4ecddd138d829
Apoorvgarg-creator <APOORVGARG.21@GMAIL.COM>
2021-05-04 05:17:20 +0530

    server and run file created

---
+++ Sequelize_TUT/batchCodegen/src/run.js
@@ -0,0 +1,19 @@
+const {db} = require('./db/models')
+const {app} = require('./server')
+
+const start = async () => {
+    try {
+        await db.sync();
+
+        app.listen(3131,()=> {
+            console.log('server started on http://localhost:3131')
+        })
+
+    }catch (e) {
+        console.error(e)
+    }
+}
+
+start();
+
+
